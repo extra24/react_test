28b76aa8f3f8f6a3293b81bd5d48b63f2ff0ea70
aram <6aram@naver.com>
2024-08-30 17:30:38 +0900

    state는 데이터가 새로 고침 될 때마다 해당페이지에서 모든 부분이 rending 됨을 보여주는 예시

--- react-app-test/src/App.js
+++ react-app-test/src/App.js
@@ -1,11 +1,15 @@
-import Button from './Button';
+import { useState } from 'react';
+// import Button from './Button';
 import styles from './App.module.css';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log('rending');
   return (
     <div>
-      <h1 className={styles.title}>Welcome back</h1>
-      <Button text={'Continue'} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
