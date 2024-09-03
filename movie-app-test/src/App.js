64c54ccda5f0409156d4280fe75c209689dcaf53
aram <6aram@naver.com>
2024-09-03 17:27:45 +0900

    Component 예제

--- movie-app-test/src/App.js
+++ movie-app-test/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import Movie from './Movie';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -34,18 +35,13 @@ function App() {
       ) : (
         <div>
           {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                <li>
-                  {movie.genres.map((g) => (
-                    <li>{g}</li>
-                  ))}
-                </li>
-              </ul>
-            </div>
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
           ))}
         </div>
       )}
