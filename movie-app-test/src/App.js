0780eb8579fa891b8067b74f0a572387038a3bd3
aram <6aram@naver.com>
2024-09-03 17:15:27 +0900

    movie list 생성

--- movie-app-test/src/App.js
+++ movie-app-test/src/App.js
@@ -1,7 +1,54 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
+      )
+    ).json();
+    // const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  // useEffect(() => {
+  //   fetch(
+  //     'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
+  //   )
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setMovies(json.data.movies);
+  //       setLoading(false);
+  //     });
+  // }, []);
+  console.log(movies);
   return (
     <div>
-      <header></header>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                <li>
+                  {movie.genres.map((g) => (
+                    <li>{g}</li>
+                  ))}
+                </li>
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
